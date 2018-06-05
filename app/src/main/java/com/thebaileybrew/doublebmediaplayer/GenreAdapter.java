bfd8b64aafad9768d7f4aa505dd6603a3e9fac64
Matthew Bailey <matthew.bailey@hthackney.com>
2018-06-05 06:10:35 -0400

    Added Genre Adapter

---
+++ app/src/main/java/com/thebaileybrew/doublebmediaplayer/GenreAdapter.java
@@ -0,0 +1,8 @@
+package com.thebaileybrew.doublebmediaplayer;
+
+import android.support.v7.widget.RecyclerView;
+
+public class GenreAdapter extends RecyclerView.Adapter {
+    //Follow this tutorial
+    //https://www.journaldev.com/13792/android-gridlayoutmanager-example
+}
